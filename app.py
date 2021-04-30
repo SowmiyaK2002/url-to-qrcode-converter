517e5dce260c8b2e826a6318844bc55988797f47
SowmiyaK2002 <sowmiyakalimuthu10@gmail>
2021-04-30 09:57:26 +0530

    added succcessfully

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template,request,jsonify,redirect,url_for
+import pyqrcode
+import png
+from pyqrcode import QRCode
+
+app = Flask(__name__)
+
+@app.route('/',methods=["POST","GET"])
+def hello_world():
+    return  render_template("index.html", title="URL TO QRCODE CONVERTER")
+
+@app.route('/downloads',methods=["POST","GET"])
+def downloads():
+    url_link = request.values.get("url","")
+    url = pyqrcode.create(url_link)
+    url.png("static\image\qrcode.png",scale=6)
+    #return ("downloaded successfully")
+    return  render_template("show.html", title="Download QR code")
+
+if __name__=="__main__":
+    app.run(debug=True)
